ae27b627b9342428b3c49f0ce2d7e0b7b2be76de
nitiraj_21 <nitirajsc21@gmail.com>
2025-11-20 17:04:05 +0530

    fix prisma version

--- prisma.config.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-export const prisma = new PrismaClient({
-  // If using direct connection (not accelerate)
-  datasourceUrl: process.env.DATABASE_URL,
-});
